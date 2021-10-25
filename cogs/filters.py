d24d7d74098b7b84ef606a5627ce4edb74ba1df5
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2021-10-25 13:01:06 +0530

    Adding invert to filter cog

--- cogs/filters.py
+++ cogs/filters.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 from io import BytesIO
 import random
 import os
@@ -11,6 +11,18 @@ class Filters(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+    @commands.command()
+    async def invert(self, ctx, member: discord.Member = None):
+        if member == None:
+            member = ctx.author
+        asset = member.avatar_url_as()
+        data = BytesIO(await asset.read())
+        profilepic = Image.open(data)
+        inverted = ImageOps.invert(profilepic)
+        inverted.save('invert.jpg', quality=100)
+
+        await ctx.channel.send(file = discord.File('invert.jpg'))
+        os.remove('invert.jpg')
 
 def setup(client):
     client.add_cog(Filters(client))
