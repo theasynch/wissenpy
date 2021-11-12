5e77306ac779d36f95ba3701486835976a396217
stxllaric.astxrism1603 <omawthankar16032007@gmail.com>
2021-11-12 18:41:54 +0530

    Update economy.py

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,14 +1,14 @@
-'''import discord
+import discord
 from discord.ext import commands
 import json
 import os
 import datetime
 import random
 
-os.chdir("https://github.com/theasynchronus/wissenpy/tree/main/cogs")
+'''os.chdir("https://github.com/theasynchronus/wissenpy/tree/main/cogs")'''
 class Economy(commands.Cog):
 
-    def __init__(self, client):
+   ''' def __init__(self, client):
         self.client = client
  
     @commands.command()
@@ -58,8 +58,7 @@ async def open_account(self,user):
 async def get_bank_data():
     with open("bank.json", "r") as f:
         users = json.load(f)
-    return users
+    return users'''
         
 def setup(client):
     client.add_cog(Economy(client))
-'''
